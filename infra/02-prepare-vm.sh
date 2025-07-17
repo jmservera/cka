713407029636a221425ca2cc7f14272f09f86823
jmservera <8036360+jmservera@users.noreply.github.com>
2025-07-17 14:13:50 +0200

    Refactor containerd setup: streamline configuration and restart process, and apply sysctl parameters.

--- infra/02-prepare-vm.sh
+++ infra/02-prepare-vm.sh
@@ -50,20 +50,17 @@ EOF
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+
 # Setup required sysctl params, these persist across reboots.
 cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
-
-# Apply sysctl params without reboot
-sudo sysctl --system
-
-sudo mkdir -p /etc/containerd
-containerd config default | sudo tee /etc/containerd/config.toml
-
-sudo systemctl restart containerd
 
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 br_netfilter
@@ -78,6 +75,8 @@ sudo sysctl --system
 git clone https://github.com/Azure/azure-container-networking && sudo ./azure-container-networking/scripts/install-cni-plugin.sh v1.6.30 v1.7.1
 
 sudo apt install iprange
+
+# sudo iptables -t nat -A POSTROUTING -m addrtype ! --dst-type local ! -d 10.0.1.0/24 -j MASQUERADE
 
 sudo iptables -t nat -A POSTROUTING -m iprange ! --dst-range 168.63.129.16 -m addrtype ! --dst-type local ! -d 192.168.0.0/24 -j MASQUERADE
 	
