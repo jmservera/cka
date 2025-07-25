d7f3d01666f72695b057cffd6ff45249c39a6615
jmservera <8036360+jmservera@users.noreply.github.com>
2025-07-25 15:15:01 +0200

    fix: update postup.sh to use VSCODE_SERVER_TOKEN for accessing VSCode

--- scripts/postup.sh
+++ scripts/postup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Environment prepared for kind cluster"
-echo "Open https://${VSCODE_FQDN}/?tkn=${VSCODE_TOKEN}&folder=/home/${ADMIN_USERNAME}/cka in your browser to access VSCode"
+echo "Open https://${VSCODE_FQDN}/?tkn=${VSCODE_SERVER_TOKEN}&folder=/home/${ADMIN_USERNAME}/cka in your browser to access VSCode"
